e3d732939f5d86ef4685c7eb04d6bb8805aedc54
EvaWayJ <ezzeribieva@yahoo.fr>
2020-06-02 13:29:00 +0200

    UI et itemdetail

--- lib/widgets/itemDetail.dart
+++ lib/widgets/itemDetail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jeveux/model/article.dart';
 import 'package:jeveux/model/item.dart';
+import 'package:jeveux/widgets/donnees_vides.dart';
 
 class ItemDetail extends StatefulWidget{
   Item item;
@@ -15,13 +17,32 @@ class ItemDetail extends StatefulWidget{
 }
 
 class _ItemDetailState extends State<ItemDetail>{
+  List<Article> articles;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: new AppBar(
         title: new Text(widget.item.nom),
-      ),
+        actions: <Widget>[
+          new FlatButton(onPressed: null, child: new Text('ajouter', style: new TextStyle(color:  Colors.white),))
+        ],),
+      body: (articles == null || articles.length == 0)?
+      new DonneesVides()
+          : new GridView.builder(
+          itemCount: articles.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context,i){
+            Article article = articles[i];
+            return new Card(
+              child: new Column(
+                children: <Widget>[
+                  new Text(article.nom)
+                ],
+              ),
+            );
+          }),
     );
   }
 
