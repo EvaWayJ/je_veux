c6ecbd2ee5762d37c2fbd7b0d77b62e0488a202c
EvaWayJ <ezzeribieva@yahoo.fr>
2020-06-02 14:21:41 +0200

    Récupérer l'article mp4

--- lib/widgets/itemDetail.dart
+++ lib/widgets/itemDetail.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jeveux/model/article.dart';
+import 'package:jeveux/model/databaseClient.dart';
 import 'package:jeveux/model/item.dart';
 import 'package:jeveux/widgets/ajout_article.dart';
 import 'package:jeveux/widgets/donnees_vides.dart';
@@ -19,7 +22,17 @@ class ItemDetail extends StatefulWidget{
 
 class _ItemDetailState extends State<ItemDetail>{
   List<Article> articles;
-
+@override
+void initState() {
+    // TODO: implement initState
+    super.initState();
+    DatabaseClient().allArticles(widget.item.id).then((list){
+      setState(() {
+        articles = list;
+      });
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -28,9 +41,15 @@ class _ItemDetailState extends State<ItemDetail>{
         title: new Text(widget.item.nom),
         actions: <Widget>[
           new FlatButton(onPressed: (){
-            Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
+             Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
               return new Ajout(widget.item.id);
-            }));
+            })).then((value){
+               DatabaseClient().allArticles(widget.item.id).then((list){
+                 setState(() {
+                   articles = list;
+                 });
+               });
+            });
           }, child: new Text('ajouter', style: new TextStyle(color:  Colors.white),))
         ],),
       body: (articles == null || articles.length == 0)?
@@ -42,8 +61,14 @@ class _ItemDetailState extends State<ItemDetail>{
             Article article = articles[i];
             return new Card(
               child: new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  new Text(article.nom)
+                  new Text(article.nom),
+                  (article.image==null)?
+                      new Image.asset("image/no_image.png")
+                      : new Image.file(new File(article.image)),
+                      new Text((article.prix == null)?"Aucun prix renseigné": "Prix: ${article.prix}"),
+                    new Text((article.magasin == null)?"Aucun magasin renseigné": "Magasin: ${article.magasin}")
                 ],
               ),
             );
